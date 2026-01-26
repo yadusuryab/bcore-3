3e6f24d84d33d463128ec5fd43cb99e6fe38a12d
Travis CI <yadusurya666@gmail.com>
2026-01-26 12:14:22 +0530

    first commit

---
+++ components/brand.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import React from "react";
+
+function Brand() {
+  return (
+    <Image src="/bcore-white-logo.png" alt="logo" width={200} height={200} />
+  );
+}
+
+export default Brand;
